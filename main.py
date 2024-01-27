5e349ebe991390ec0d3b7e41fc7a534c925f61db
argocdintpoc <argocdintpoc@adobe.com>
2024-01-27 17:01:29 -0500

    Thumbnail in a webpave

---
+++ main.py
@@ -0,0 +1,38 @@
+import streamlit as st
+import pandas
+
+st.set_page_config(layout="wide")
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/photo.png")
+
+with col2:
+    st.title("Ardit Sulce")
+    content = """
+        Hi, I am Ardit! I am a Python programmer, teacher, and founder of PythonHow. I graduated in 2013 with a Master of Science in Geospatial Technologies from the University of Muenster in Germany with a focus on using Python for remote sensing.
+    I have worked with companies from various countries, such as the Center for Conservation Geography, to map and understand Australian ecosystems, image processing with the Swiss in-Terra, and performing data mining to gain business insights with the Australian Rapid Intelligence.
+        """
+    st.info(content)
+
+content2 = """
+Below you can find some of the apps i have written in python. Feel free to contact me 
+"""
+st.write(content2)
+
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
+
+with col3:
+    df = pandas.read_csv("data.csv", sep=";")
+    for index, row in df[:10].iterrows():
+        st.title(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source code]({row['url']})")
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.title(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source code]({row['url']})")
